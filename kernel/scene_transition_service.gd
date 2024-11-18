edaa6d0efeb16b1f369384951bb8e96a9ff2a469
Alexander Getmansky <alex@getsky.games>
2024-11-18 15:59:50 +0300

    feat: added scene transition service (#9)

---
+++ kernel/scene_transition_service.gd
@@ -0,0 +1,35 @@
+class_name SceneTransitionService extends Object
+
+enum CleanType {DELETE, KEEP_RUNNING, REMOVE}
+
+var _current_ui: Control
+
+var transition_screen_handler: TransitionScreenHandler
+var world : Node2D
+var ui : Control
+
+func change(new_scene: String, type: CleanType = CleanType.DELETE) -> void:
+	if _current_ui != null:
+		match type:
+			CleanType.DELETE:
+				_current_ui.queue_free()
+			
+			CleanType.KEEP_RUNNING:
+				_current_ui.visible = false
+			
+			CleanType.REMOVE:
+				ui.remove_child(_current_ui)
+
+	var new = load(new_scene).instantiate()
+	ui.add_child(new)
+	_current_ui = new
+
+func transit(new_scene: String, type: CleanType = CleanType.DELETE) -> void:
+	if transition_screen_handler:
+		transition_screen_handler.invoke()
+		await transition_screen_handler.animator.animation_finished
+		
+	change(new_scene, type)
+	
+	if  transition_screen_handler:
+		transition_screen_handler.invoke(true)
---
+++ kernel/service_locator.gd
@@ -0,0 +1,3 @@
+extends Node
+
+var scene_transition_service : SceneTransitionService
