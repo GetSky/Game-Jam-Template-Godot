442f243108fcb4f3b2ab3e6bff499945145a0ab9
Alexander Getmansky <alex@getsky.games>
2024-11-21 17:57:45 +0300

    feat: implemented the injector instead of the service locator (#16)

--- kernel/kernel.gd
+++ kernel/kernel.gd
@@ -2,16 +2,13 @@ class_name Kernel extends Node
 
 @export var world : Node2D
 @export var ui : Control
-@export var transition_screen_handler : TransitionScreenHandler
 @export var splash_screen : PackedScene
 
-func _ready() -> void:
-	init()
-	ServiceLocator.scene_transition_service.set_next_ui(splash_screen).change()
+@onready var sts: SceneTransitionService = Injector.provide(
+	SceneTransitionService, 
+	"root", 
+	[world, ui],
+	)
 
-func init() -> void:
-	var service = SceneTransitionService.new()
-	service.transition_screen_handler = transition_screen_handler
-	service.world = world
-	service.ui = ui
-	ServiceLocator.scene_transition_service = service
+func _ready() -> void:
+	sts.set_next_ui(splash_screen).change()
