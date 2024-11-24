02234ab14b2a32e2e99b89a9eb70b037b7c9fd7a
Alexander Getmansky <alex@getsky.games>
2024-11-24 20:05:28 +0300

    feat (splash screen): added background sound (#20)

--- features/splash_screen/splash_screen.gd
+++ features/splash_screen/splash_screen.gd
@@ -1,6 +1,5 @@
 class_name SplashScreen extends Control
 
-@export var splash_screens_container : Node
 @export var in_time_sec : float = 0.5
 @export var fade_in_time_sec : float = 1.5
 @export var pause_time_sec : float = 1.5
@@ -9,15 +8,20 @@ class_name SplashScreen extends Control
 @export var load_ui_scene : PackedScene
 @export var load_world_scene : PackedScene
 
+@onready var sound = $BackgroundSound
+@onready var splash_screens_container = $Splashes
+
 @onready var transition_service: SceneTransitionService = Injector.inject(SceneTransitionService)
 
 var splash_screens : Array
 var _tween : Tween
 
+
 func _ready() -> void:
-	get_screens()
+	_get_screens()
+	sound.play()
 	fade()
-	
+
 
 func fade() -> void:
 	for i in len(splash_screens):
@@ -26,10 +30,11 @@ func fade() -> void:
 		if i > 0:
 			splash_screens[i].modulate.a = 0.0
 			_tween.tween_interval(in_time_sec)
+			_tween.tween_callback(sound.play)
 			_tween.tween_property(splash_screens[i], "modulate:a", 1.0, fade_in_time_sec)
 		
 		_tween.tween_interval(pause_time_sec)
-
+		
 		if (i < len(splash_screens) - 1):
 			_tween.tween_property(splash_screens[i], "modulate:a", 0.0, fade_out_time_sec)
 			_tween.tween_interval(out_time_sec)
@@ -38,11 +43,12 @@ func fade() -> void:
 	
 	transition_service.set_next_world(load_world_scene).set_next_ui(load_ui_scene).transit()
 
-func get_screens() -> void:
-	splash_screens = splash_screens_container.get_children()
 
+func _get_screens() -> void:
+	splash_screens = splash_screens_container.get_children()
 	for i in len(splash_screens):
 		splash_screens[i].modulate.a = 1.0 if i == 0 else 0.0
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_pressed():
