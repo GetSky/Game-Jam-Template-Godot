ad8212722b4c15b7f558995bba724cc58efa53f8
Alexander Getmansky <alex@getsky.games>
2024-12-01 17:19:46 +0300

    refactored service

--- features/scene_transition/scene_transition_service.gd
+++ features/scene_transition/scene_transition_service.gd
@@ -11,9 +11,13 @@ var _last_ui: PackedScene
 var _last_world: PackedScene
 var _next_ui: PackedScene
 var _next_world: PackedScene
+var _type_ui: CleanType
+var _type_world: CleanType
 
 var _current_ui: Control
+var _current_ui_prefinished = false
 var _current_world: Node2D
+var _current_world_prefinished = false
 
 var _keep_scenes : Dictionary = {}
 
@@ -25,42 +29,53 @@ func _init(root: Node, world: Node2D, ui: Control):
 	root.add_child(_transition_screen, true)
 
 
-func set_next_ui(new_ui: PackedScene) -> SceneTransitionService:
+func set_next_ui(new_ui: PackedScene, type: CleanType = CleanType.DELETE) -> SceneTransitionService:
 	_next_ui = new_ui
+	_type_ui = type
 	return self
 
 
-func set_next_world(new_world: PackedScene) -> SceneTransitionService:
+func set_next_world(new_world: PackedScene, type: CleanType = CleanType.DELETE) -> SceneTransitionService:
 	_next_world = new_world
+	_type_world = type
 	return self
 
 
-func change(type: CleanType = CleanType.DELETE) -> void:
+func change() -> void:
 	if _isEqual(_last_ui, _next_ui) == false:
-		_change_ui(type)
-		
+		_change_ui()
+	
 	if _isEqual(_last_world, _next_world) == false:
-		_change_world(type)
+		_change_world()
 
 
-func transit(type: CleanType = CleanType.DELETE) -> void:
+func transit() -> void:
 	if _transition_screen:
 		await _transition_screen.invoke()
 	
-	change(type)
+	change()
 	
 	if  _transition_screen:
 		_transition_screen.invoke(true)
 
 
-func _change_ui(type: CleanType) -> void:
-	_clear(_current_ui, _ui, type)
+func finish_pre_hide(scene: Node) -> void:
+	if scene == _current_ui:
+		_current_ui_prefinished = true
+	elif scene == _current_world:
+		_current_world_prefinished = true
+	if (_current_ui_prefinished || _current_ui == null) && (_current_world_prefinished || _current_world == null):
+		return
+
+
+func _change_ui() -> void:
+	_clear(_current_ui, _ui, _type_ui)
 	_current_ui = _create(_next_ui, _ui)
 	_last_ui = _next_ui
 
 
-func _change_world(type: CleanType) -> void:
-	_clear(_current_world, _world, type)
+func _change_world() -> void:
+	_clear(_current_world, _world, _type_world)
 	_current_world = _create(_next_world, _world)
 	_last_world = _next_world
 
@@ -68,6 +83,8 @@ func _change_world(type: CleanType) -> void:
 func _clear(target: Node, root: Node, type: CleanType) -> void:
 	if target == null: 
 		return
+
+	# target.pre_hide(self.finish_pre_hide)
 
 	match type:
 		CleanType.DELETE:
