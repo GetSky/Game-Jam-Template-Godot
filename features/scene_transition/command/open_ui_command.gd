b86e02b9012816efe3451cff0bffd1e1c300ddcf
Alexander Getmansky <alex@getsky.games>
2024-12-12 00:05:33 +0300

    removed old commands

--- features/scene_transition/command/open_ui_command.gd
+++
@@ -1,9 +0,0 @@
-class_name OpenUICommand extends Injectable
-
-var _service: UITransitionService 
-
-func _init(s: UITransitionService) -> void:
-	_service = s
-
-func invoke(ui: PackedScene) -> void:
-	_service.change(ui)
